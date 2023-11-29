8df6becd59375fb5d21f4e871249096bac99f808
Sokrat Magomedov <oooosokrat@mail.ru>
2023-11-29 14:33:07 +0300

    Ex_002 and 003 added

---
+++ Ex_002/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Задайте одномерный массив из 10 целых чисел от 1 до 100. 
+Найдите количество элементов массива, значения которых лежат в отрезке [20,90] */
+
+int [] CreateArray (int size, int min, int max)
+{
+    int [] array = new int [size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next (min, max + 1);
+    }
+    return array;
+}
+
+void ShowArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write ($"{array [i]} ");
+    }
+}
+
+int CountNumber (int [] array, int min, int max)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] >= min && array [i] <= max) count++;
+    }
+    return count; 
+}
+
+int size = 10;
+int min = 1;
+int max = 100;
+int minSearch = 20;
+int maxSearch = 90;
+int [] array = CreateArray (size, min, max);
+ShowArray (array);
+Console.WriteLine ();
+int count = CountNumber (array, minSearch, maxSearch);
+Console.WriteLine ($"Количество чисел на отрезке [20; 90] в массиве равно {count}");
---
+++ Ex_003/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Задайте массив на 10 целых чисел. 
+Напишите программу, которая определяет количество чётных чисел в массиве. */
+
+int [] CreateArray (int size, int min, int max)
+{
+    int [] array = new int [size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next (min, max + 1);
+    }
+    return array;
+}
+
+void ShowArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write ($"{array [i]} ");
+    }
+}
+
+int CountEven (int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] % 2 == 0) count++;
+    }
+    return count; 
+}
+
+Console.WriteLine ("Enter array min");
+int min = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Enter array max");
+int max = Convert.ToInt32 (Console.ReadLine ());
+int [] array = CreateArray (10, min, max);
+ShowArray (array);
+Console.WriteLine ();
+int count = CountEven (array);
+Console.WriteLine ($"The amount of even numbers is {count}");
